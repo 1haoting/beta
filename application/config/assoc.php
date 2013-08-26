75528c6ac5f91e817e7f50e675379bf59389de4c
leohao <blueseamyheart@hotmail.com>
2013-08-26 23:42:32 +0800

    关系表配置文件

---
+++ application/config/assoc.php
@@ -0,0 +1,21 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+/*
+| -------------------------------------------------------------------
+| MOVIE CINEMA ASSOC
+| -------------------------------------------------------------------
+| This file will contain the settings needed to access your assoc movie 
+| config
+| -------------------------------------------------------------------
+*/
+
+//movie type
+$typeConfig[0] = '2D';
+$typeConfig[1] = '3D';
+$typeConfig[2] = 'IMAX';
+$typeConfig[3] = '巨幕';
+$typeConfig[4] = '数字';
+
+//movie language
+$language[0] = '原声';
+$language[1] = '普通话';
+$language[2] = '其他';
